3c9222dc6f8ba2756d8f2fc0be599b4a2230480f
2568808909 <2568808909@qq.com>
2020-02-23 16:50:31 +0800

    avl

--- src/main/java/datastruce/AVLTree.java
+++ src/main/java/datastruce/AVLTree.java
@@ -1,90 +1,155 @@
 package datastruce;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 平衡二叉树
- *
- * @param <T>
+ * 最早的自平衡二分搜索结构
+ * 满二叉树：除了叶子结点外，其他的所有节点都有左右两颗子树
+ * 完全二叉树：非叶子节点的右子树可能为空，但左子树一定存在
+ * 平衡二叉树：对于任意一个节点，其左子树与右子树的高度相差不能超过1
  */
-public class AVLTree<T extends Comparable> {
+public class AVLTree<K extends Comparable<K>, V> {
 
-    private class AVLNode {
+    private class Node {
+        private K key;
+        private V value;
+        private Node left, right;
+        private int height; //当前节点高度值
 
-        private T data;
-        private AVLNode left;
-        private AVLNode right;
-        private boolean console;
-        private int high;
-
-        public AVLNode(T data) {
-            this.data = data;
-            this.console = false;
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
             this.left = null;
             this.right = null;
-            this.high = 0;
+            height = 1;
         }
+
+
     }
 
-    private AVLNode root;
+    private Node root;
     private int size;
 
-    public void add(T data) {
-        if (root == null) {
-            root = new AVLNode(data);
+    AVLTree() {
+        this.root = null;
+        this.size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private int getHeight(Node node) {
+        return node == null ? 0 : node.height;
+    }
+
+    public void add(K key, V value) {
+        root = add(key, value, root);
+    }
+
+    private Node add(K key, V value, Node node) {
+        if (node == null) {
+            size++;
+            return new Node(key, value);
+        }
+        K current = node.key;
+        if (key.compareTo(current) < 0) {
+            node.left = add(key, value, node.left);
+        } else if (key.compareTo(current) > 0) {
+            node.right = add(key, value, node.right);
         } else {
-            add(root, data);
+            node.value = value;
         }
-    }
+        //更新高度
+        node.height = Math.max(getHeight(node.left), getHeight(node.right)) + 1;
+        //计算平衡因子
+        int balanceFactor = getBalanceFactor(node);
 
-    private void add(AVLNode avlNode, T data) {
-        T current = avlNode.data;
-        if (data.compareTo(current) < 0) {
-            if (avlNode.left == null) {
-                avlNode.left = new AVLNode(data);
-                size++;
-            } else {
-                add(avlNode.left, data);
-            }
-        } else if (data.compareTo(current) > 0) {
-            if (avlNode.right == null) {
-                avlNode.right = new AVLNode(data);
-                size++;
-            } else {
-                add(avlNode.right, data);
-            }
-        } else {
-            throw new RuntimeException("已有相等元素在此树中");
+        //不满足平衡二叉树条件
+        if (balanceFactor > 1 && getBalanceFactor(node.left) >= 0) {//LL 进行一次右旋转
+            return rightRotate(node);
+        } else if (balanceFactor > 1 && getBalanceFactor(node.left) < 0) {//LR 对左子树进行一次左旋转后，自身进行一次进行一次右旋转
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
+        } else if (balanceFactor < -1 && getBalanceFactor(node.right) <= 0) {//RR 进行一次左旋转
+            return leftRotate(node);
+        } else if (balanceFactor < -1 && getBalanceFactor(node.right) > 0) {//RL 对右子树进行一次右旋转后，自身进行一次进行一次左旋转
+            node.right = rightRotate(node.right);
+            return leftRotate(node);
         }
-        avlNode.high++;
-        int balanceFactor = getBalanceFactor(avlNode);
-        if (Math.abs(balanceFactor) > 2) {
-            if (balanceFactor > 0) {
 
-            } else {
+        return node;
+    }
 
-            }
+    private int getBalanceFactor(Node node) {
+        return node == null ? 0 : getHeight(node.left) - getHeight(node.right);
+    }
+
+    private Node leftRotate(Node node) {
+        Node result = node.right;
+        Node lLeft = result.left;
+
+        result.left = node;
+        node.right = lLeft;
+        //更新高度
+        node.height = Math.max(getHeight(node.left), getHeight(node.right)) + 1;
+        result.height = Math.max(getHeight(result.left), getHeight(result.right)) + 1;
+        return result;
+    }
+
+    private Node rightRotate(Node node) {
+        Node result = node.left;
+        Node rRight = result.right;
+        result.right = node;
+        node.left = rRight;
+        //更新高度
+        node.height = Math.max(getHeight(node.left), getHeight(node.right)) + 1;
+        result.height = Math.max(getHeight(result.left), getHeight(result.right)) + 1;
+        return result;
+    }
+
+    //判断该二叉树是否为二分搜索树
+    public boolean isBST() {
+        List<K> list = new ArrayList<>();
+        inOrder(root, list);
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i).compareTo(list.get(i - 1)) <= 0)
+                return false;
         }
+        return true;
     }
 
-    private void rightRotate(AVLNode avlNode) {
-
+    /**
+     * 判断该树是否平衡
+     *
+     * @return
+     */
+    public boolean isBalance() {
+        return isBalance(root);
     }
 
-    private void leftRotate(AVLNode avlNode) {
 
-    }
-
-    private int getBalanceFactor(AVLNode avlNode) {
-        if (avlNode == null) {
-            return 0;
+    private boolean isBalance(Node node) {
+        if (node == null) return true;
+        int balanceFactor = getBalanceFactor(node);
+        if (Math.abs(balanceFactor) > 1) {
+            return false;
         }
-        return getHigh(avlNode.left) - getHigh(avlNode.right);
+        return isBalance(node.left) && isBalance(node.right);
     }
 
-    private int getHigh(AVLNode avlNode) {
-        if (avlNode == null) {
-            return 0;
+    private void inOrder(Node node, List<K> keys) {
+        if (node == null) {
+            return;
         }
-        return avlNode.high;
+        inOrder(node.left, keys);
+        keys.add(node.key);
+        inOrder(node.right, keys);
     }
-
 }
--- src/main/java/datastruce/Test.java
+++ src/main/java/datastruce/Test.java
@@ -2,18 +2,17 @@ package datastruce;
 
 public class Test {
     public static void main(String[] args) {
-        BinarySearchTree<Integer> tree=new BinarySearchTree<>();
-        tree.add(5);
-        tree.add(3);
-        tree.add(6);
-        tree.add(2);
-        tree.add(4);
-        tree.add(8);
-        tree.add(7);
+        AVLTree<Integer,Integer> tree=new AVLTree<>();
+        tree.add(12,1);
+        tree.add(8,1);
+        tree.add(18,1);
+        tree.add(5,1);
+        tree.add(11,1);
+        tree.add(3,1);
+        tree.add(7,1);
+        tree.add(2,1);
 
-        tree.preOrderNR();
-        tree.remove(9);
-        System.out.println();
-        tree.preOrderNR();
+        System.out.println(tree.isBalance());
+        System.out.println(tree.isBST());
     }
 }
