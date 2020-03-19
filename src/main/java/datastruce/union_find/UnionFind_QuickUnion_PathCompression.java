a150e61603247a68c47c4bfcca4a65e7c55482cb
2568808909 <2568808909@qq.com>
2020-03-19 15:37:17 +0800

    路径压缩

--- src/main/java/datastruce/union_find/UnionFind_QuickUnion_PathCompression.java
+++ src/main/java/datastruce/union_find/UnionFind_QuickUnion_PathCompression.java
@@ -2,12 +2,12 @@ package datastruce.union_find;
 
 /**
  * 路径压缩
- *
+ * <p>
  * 主要是在find操作时进行优化(parent[index]=parent[parent[index]])，在find操作的同时逐渐降低树的高度
  * 对于rank数组，其实可以不需要维护，rank之所以不被称作高度也是因为如此，更准确来说rank数组只是一个参考值，
  * 即使不太准确，后来也有路径压缩进行优化。
  */
-public class UnionFind_QuickUnion_PathCompression implements UF{
+public class UnionFind_QuickUnion_PathCompression implements UF {
     private int[] parent;
     private int[] rank;
 
@@ -59,6 +59,24 @@ public class UnionFind_QuickUnion_PathCompression implements UF{
         }
         return index;
     }
+
+//    /**
+//     * 通过递归实现路径压缩，与上面的方式比较，这个可以在查找过程中，将沿途的节点都指向根节点，
+//     * 而上面的实现方式要将所有的节点都直接指向根节点可能要多几次操作。但是，由于递归其实消耗
+//     * 比循环大，所以这种实现方式可能会比上面的稍微慢一点。
+//     *
+//     * @param index
+//     * @return
+//     */
+//    private int find(int index) {
+//        if (index < 0 || index > parent.length) {
+//            throw new IndexOutOfBoundsException("index out of bound");
+//        }
+//        if (index != parent[index]) {
+//            parent[index] = find(parent[index]);
+//        }
+//        return index;
+//    }
 
     @Override
     public int getSize() {
